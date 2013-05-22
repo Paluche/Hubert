520adb315d04a0128ab340e308d2468c25afaf3a
Hubert Lefevre <hubelefevre@gmail.com>
2013-05-22 11:25:23 +0200

    Add of ChibiOS

---
+++ ChibiOS/testhal/STM32F1xx/UART/main.c
@@ -0,0 +1,88 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006,2007,2008,2009,2010,
+                 2011,2012,2013 Giovanni Di Sirio.
+
+    This file is part of ChibiOS/RT.
+
+    ChibiOS/RT is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    ChibiOS/RT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ch.h"
+#include "hal.h"
+
+static VirtualTimer vt1, vt2;
+
+static void restart(void *p) {
+
+  (void)p;
+
+  chSysLockFromIsr();
+  uartStartSendI(&UARTD2, 14, "Hello World!\r\n");
+  chSysUnlockFromIsr();
+}
+
+static void ledoff(void *p) {
+
+  (void)p;
+  palSetPad(IOPORT3, GPIOC_LED);
+}
+
+/*
+ * UART driver configuration structure.
+ */
+static UARTConfig uart_cfg_1 = {
+  txend1,
+  txend2,
+  rxend,
+  rxchar,
+  rxerr,
+  38400,
+  0,
+  USART_CR2_LINEN,
+  0
+};
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+  /*
+   * System initializations.
+   * - HAL initialization, this also initializes the configured device drivers
+   *   and performs the board-specific initializations.
+   * - Kernel initialization, the main() function becomes a thread and the
+   *   RTOS is active.
+   */
+  halInit();
+  chSysInit();
+
+  /*
+   * Activates the serial driver 2 using the driver default configuration.
+   */
+  uartStart(&UARTD2, &uart_cfg_1);
+
+  /*
+   * Starts the transmission, it will be handled entirely in background.
+   */
+  uartStartSend(&UARTD2, 13, "Starting...\r\n");
+
+  /*
+   * Normal main() thread activity, in this demo it does nothing.
+   */
+  while (TRUE) {
+    chThdSleepMilliseconds(500);
+  }
+  return 0;
+}
