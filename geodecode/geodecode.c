96515bc4b89274230a9505037c0edbb3a529ecad
hlefevre <hubert.lefevre@netatmo.com>
2013-10-07 18:42:08 +0200

    Program to decode the coded phrase in geo-catching

---
+++ geodecode/geodecode.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+  if(argc == 1) {
+    printf("Error, usage %s <text>\n", argv[0]);
+    return -1;
+  }
+
+
+  int i;
+  int u;
+  for (u = 1; u < argc; u++) {
+    char *text = argv[u];
+    for (i = 0; text[i]; i++) {
+      unsigned char letter = text[i];
+      if (((letter >= 'a') && (letter < 'n')) ||
+          ((letter >= 'A') && (letter < 'N'))) {
+        letter += 13;
+      } else if (((letter >= 'n') && (letter <= 'z')) ||
+                 ((letter >= 'N') && (letter <= 'Z'))) {
+        letter -= 13;
+      } else {
+       // Special characters : do nothing
+      }
+        printf("%c", letter);
+    }
+    printf(" ");
+  }
+  printf("\n");
+  return 0;
+}
